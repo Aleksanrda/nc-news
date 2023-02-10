c6e2167860e208305da360c4b7d0997800000bfd
Aleksanrda <kryvkoalexandra05@gmail.com>
2023-02-10 12:53:14 +0000

    add style for ErrorPage

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,10 +1,19 @@
 import { Link } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
+import donut from '../img/donut.png';
 
 const ErrorPage = () => {
     return (
-        <div>
-            <div className="non-existent-path">404</div>
-            <Link to={`/`}>Back to your articles!</Link>
+        <div className="non-existent-path">
+            <div className="float-child">
+                <h1>WHOOPS!</h1>
+                <h4>We couldn't find the page you are looking for :(</h4>
+                <Button variant="secondary" className='button-path-to-articles'><Link to={`/`} className="path-to-articles">Back to your articles!</Link></Button>
+            </div>
+            <div className="float-child">
+                <img src={donut} alt=""></img>
+            </div>
+            
         </div>
     );
 };
